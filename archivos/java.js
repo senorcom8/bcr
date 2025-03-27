a0ba2d72e5d0aaa35adcf7d1d323fdcf326a76af
senorcom8 <senorcom88@gmail.com>
2025-03-27 15:29:06 -0500

    Add files via upload

---
+++ archivos/java.js
@@ -0,0 +1,48 @@
+
+button1.disabled = true;
+
+
+
+			function ACTION2() {
+  if (pass.value.length=== 8){
+    button1.disabled = false
+	 button1.style.background = "#df3d3d";
+  }
+   
+}			function ACTION3() {
+  if (pass.value.length=== 8){
+    button1.disabled = false
+	 button1.style.backgroundColor = "#df3d3d";
+  }
+   
+}
+
+			function ACTION222() {
+  if (pass.value.length=== 4){
+    button1.disabled = false
+	 button1.style.background = "#df3d3d";
+  }
+   
+}			function ACTION333() {
+  if (pass.value.length=== 4){
+    button1.disabled = false
+	 button1.style.backgroundColor = "#df3d3d";
+  }
+   
+}
+				
+				
+				  function pass1(){ var contenedor = document.getElementById("imgpass1");	 imgpass1.style.display = "none";  return true;	}
+		  
+		  function pass2(){ var contenedor = document.getElementById("imgpass2");	 imgpass2.style.display = "block";  return true;	}
+			
+			function pass3(){ var contenedor = document.getElementById("imgpass2");	 imgpass2.style.display = "none";  return true;	}
+		  
+		  function pass4(){ var contenedor = document.getElementById("imgpass1");	 imgpass1.style.display = "block";  return true;	}
+				
+		  function pass11(){ var contenedor = document.getElementById("pass").type="text";}
+
+			function pass33(){ var contenedor = document.getElementById("pass").type="password";}
+				
+				
+				
